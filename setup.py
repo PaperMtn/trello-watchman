fbc816be6bc0b549e83975692625aa1c56765b2d
PaperMtn <andrewbyford86@hotmail.com>
2021-05-06 17:42:24 +0100

    Updated Python version support

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
     author=a.__author__,
     author_email=a.__email__,
